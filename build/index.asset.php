a06c123a3d6c42aa680489b16f53d92da429d09f
André Gregório <andregn26@gmail.com>
2023-03-16 11:29:05 +0000

    Adding block props

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '186c7bd853d5f050c586');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b30725a01fabb55a3dcb');
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import { registerBlockType } from "@wordpress/blocks";
-import { RichText } from "@wordpress/block-editor";
+import { RichText, useBlockProps } from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
 import block from "./block.json";
 
 registerBlockType(block.name, {
     edit({ attributes, setAttributes }) {
-        // console.log("🚀 ~ file: index.js:8 ~ edit ~ props:", props);
         const { content } = attributes;
+        const blockProps = useBlockProps();
+        // console.log("🚀 ~ file: index.js:11 ~ edit ~ blockProps:", blockProps);
 
         return (
             <RichText
+                {...blockProps}
                 tagName="h2"
                 placeholder={__("Enter Heading", "udemy-plus")}
                 value={content}
@@ -19,6 +21,9 @@ registerBlockType(block.name, {
     },
     save({ attributes }) {
         const { content } = attributes;
-        return <RichText.Content tagName="h2" value={content} />;
+        const blockProps = useBlockProps.save();
+        return (
+            <RichText.Content {...blockProps} tagName="h2" value={content} />
+        );
     },
 });
