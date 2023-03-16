1a153df2f7488726ed1528bc3d27ba005ab9a249
André Gregório <andregn26@gmail.com>
2023-03-16 20:41:29 +0000

    The PanelColorSettings Component

--- build/blocks/search-form/index.asset.php
+++ build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '773f9b17582df40095ee');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1c37b9cd99b12184288d');
