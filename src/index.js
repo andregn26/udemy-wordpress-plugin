6dc7c5566be3e3a275d2df8779882ec63dc10d19
André Gregório <andregn26@gmail.com>
2023-03-16 11:15:24 +0000

    Saving the block

--- src/index.js
+++ src/index.js
@@ -17,4 +17,8 @@ registerBlockType(block.name, {
             />
         );
     },
+    save({ attributes }) {
+        const { content } = attributes;
+        return <RichText.Content tagName="h2" value={content} />;
+    },
 });
