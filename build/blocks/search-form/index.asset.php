b81170362a629e3f7e926f5635ea8ad3f77bb10f
André Gregório <andregn26@gmail.com>
2023-03-16 19:54:19 +0000

    Adding the Template

--- build/blocks/search-form/index.asset.php
+++ build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '907f33b03eecbf560ef8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '773f9b17582df40095ee');
--- src/blocks/search-form/index.js
+++ src/blocks/search-form/index.js
@@ -1,10 +1,24 @@
 import { registerBlockType } from "@wordpress/blocks";
+import { useBlockProps } from "@wordpress/block-editor";
 import block from "./block.json";
+import "./main.css";
 import icons from "../../icons";
 
 registerBlockType(block.name, {
     icon: icons.primary,
     edit() {
-        return <h1>Testteeee</h1>;
+        const blockProps = useBlockProps();
+        return (
+            <div {...blockProps}>
+                <h1>Search: Your search term here</h1>
+                <form>
+                    <input type="text" placeholder="Search" />
+
+                    <div className="btn-wrapper">
+                        <button type="submit">Search</button>
+                    </div>
+                </form>
+            </div>
+        );
     },
 });
