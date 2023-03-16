ad1062e19ea42c1e12020bd98dabde32a00645c7
André Gregório <andregn26@gmail.com>
2023-03-16 12:54:58 +0000

    fixed bug

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1d6587cc1d329277b955');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5caf84511c400d7fb4bf');
