41967f7b032b5921c4b8a0f7d124245930bcc842
André Gregório <andregn26@gmail.com>
2023-03-16 21:29:24 +0000

    Creating a Render Callback

--- src/blocks/search-form/index.js
+++ src/blocks/search-form/index.js
@@ -1,8 +1,8 @@
 import { registerBlockType } from "@wordpress/blocks";
 import {
-    useBlockProps,
-    PanelColorSettings,
-    InspectorControls,
+	useBlockProps,
+	PanelColorSettings,
+	InspectorControls,
 } from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
 import block from "./block.json";
@@ -10,56 +10,60 @@ import "./main.css";
 import icons from "../../icons";
 
 registerBlockType(block.name, {
-    icon: icons.primary,
-    edit({ attributes, setAttributes }) {
-        const { bgColor, textColor } = attributes;
-        const blockProps = useBlockProps({
-            style: {
-                "background-color": bgColor,
-                color: textColor,
-            },
-        });
+	icon: icons.primary,
+	edit({ attributes, setAttributes }) {
+		const { bgColor, textColor } = attributes;
+		const blockProps = useBlockProps({
+			style: {
+				"background-color": bgColor,
+				color: textColor,
+			},
+		});
 
-        return (
-            <>
-                <InspectorControls>
-                    <PanelColorSettings
-                        title={__("Colors", "udemy-plus")}
-                        colorSettings={[
-                            {
-                                label: __("Background Color", "udemy-plus"),
-                                value: bgColor,
-                                onChange: (newValue) =>
-                                    setAttributes({ bgColor: newValue }),
-                            },
-                            {
-                                label: __("Text Color", "udemy-plus"),
-                                value: textColor,
-                                onChange: (newValue) =>
-                                    setAttributes({ textColor: newValue }),
-                            },
-                        ]}
-                    />
-                </InspectorControls>
-                <div {...blockProps}>
-                    <h1>Search: Your search term here</h1>
-                    <form>
-                        <input type="text" placeholder="Search" />
+		return (
+			<>
+				<InspectorControls>
+					<PanelColorSettings
+						title={__("Colors", "udemy-plus")}
+						colorSettings={[
+							{
+								label: __("Background Color", "udemy-plus"),
+								value: bgColor,
+								onChange: (newValue) =>
+									setAttributes({
+										bgColor: newValue,
+									}),
+							},
+							{
+								label: __("Text Color", "udemy-plus"),
+								value: textColor,
+								onChange: (newValue) =>
+									setAttributes({
+										textColor: newValue,
+									}),
+							},
+						]}
+					/>
+				</InspectorControls>
+				<div {...blockProps}>
+					<h1>Search: Your search term here</h1>
+					<form>
+						<input type="text" placeholder="Search" />
 
-                        <div className="btn-wrapper">
-                            <button
-                                style={{
-                                    "background-color": bgColor,
-                                    color: textColor,
-                                }}
-                                type="submit"
-                            >
-                                Search
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </>
-        );
-    },
+						<div className="btn-wrapper">
+							<button
+								style={{
+									"background-color": bgColor,
+									color: textColor,
+								}}
+								type="submit"
+							>
+								Search
+							</button>
+						</div>
+					</form>
+				</div>
+			</>
+		);
+	},
 });
