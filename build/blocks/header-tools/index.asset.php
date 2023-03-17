c1639c6c0519806c1f8ab5b355d55aaeb15472ea
André Gregório <andregn26@gmail.com>
2023-03-17 18:37:47 +0000

    The CheckboxControl Component

--- build/blocks/header-tools/index.asset.php
+++ build/blocks/header-tools/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '65d6a12705e0e7b21d60');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e5df40a875761bffef2e');
--- src/blocks/header-tools/index.js
+++ src/blocks/header-tools/index.js
@@ -1,6 +1,10 @@
 import { registerBlockType } from "@wordpress/blocks";
 import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, SelectControl } from "@wordpress/components";
+import {
+	PanelBody,
+	SelectControl,
+	CheckboxControl,
+} from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
 import icons from "../../icons.js";
 import "./main.css";
@@ -26,6 +30,18 @@ registerBlockType("udemy-plus/header-tools", {
 							]}
 							onChange={(newValue) =>
 								setAttributes({ showAuth: newValue === "true" })
+							}
+						/>
+						<CheckboxControl
+							label={__("Show Login/Register Link", "udemy-plus")}
+							help={
+								showAuth
+									? __("Showing Link", "udemy-plus")
+									: __("Hiding Link", "udemy-plus")
+							}
+							checked={showAuth}
+							onChange={(newValue) =>
+								setAttributes({ showAuth: newValue })
 							}
 						/>
 					</PanelBody>
