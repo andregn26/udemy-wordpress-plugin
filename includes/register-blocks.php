9062576966a8146c0e984dcbc8a84555e8429f32
André Gregório <andregn26@gmail.com>
2023-03-17 12:13:19 +0000

    Creating a Page Header Block

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -4,11 +4,13 @@ function up_register_blocks()
 {
     $blocks = [
         ["name" => "fancy-header"],
-        ["name" => "search-form", "options" => ["render_callback" => "up_search_form_render_cb"]]
+        ["name" => "search-form", "options" => ["render_callback" => "up_search_form_render_cb"]],
+        ["name" => "page-header", "options" => ["render_callback" => "up_page-header_render_cb"]]
     ];
     foreach ($blocks as $block) {
         register_block_type(
-            UP_PLUGIN_DIR . "build/blocks/" . $block["name"], isset($block["options"]) ? $block["options"] : [] 
+            UP_PLUGIN_DIR . "build/blocks/" . $block["name"],
+            isset($block["options"]) ? $block["options"] : []
         );
     }
 }
