cf923a8ee51152b5908542510956b04b0505c6dc
André Gregório <andregn26@gmail.com>
2023-04-05 15:29:17 +0100

    Adding Columns

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -8,7 +8,9 @@ function up_register_blocks()
         ["name" => "page-header", "options" => ["render_callback" => "up_page_header_render_cb"]],
         ["name" => "header-tools", "options" => ["render_callback" => "up_header_tools_cb"]],
         ["name" => "auth-modal", "options" => ["render_callback" => "up_auth_modal_cb"]],
-        ["name" => "recipe-summary", "options" => ["render_callback" => "up_recipe_summary_render_cb"]]
+        ["name" => "recipe-summary", "options" => ["render_callback" => "up_recipe_summary_render_cb"]],
+        ["name" => "team-members-group"],
+        ["name" => "team-member"]
     ];
     foreach ($blocks as $block) {
         register_block_type(
