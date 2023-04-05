419e7cf86d1fadc97fb72b1d363a128e4e98506d
André Gregório <andregn26@gmail.com>
2023-04-05 15:49:58 +0100

    Custom Image Sizes

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1c14d07b9538b7d9c865');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b47fa7fb126a979b74c7');
