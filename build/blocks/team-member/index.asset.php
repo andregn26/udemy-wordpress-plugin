185e4f8277b0352a5fc89f76568f3ad72157fc16
André Gregório <andregn26@gmail.com>
2023-04-05 16:00:10 +0100

    Handling Selections

--- build/blocks/team-member/index.asset.php
+++ build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b47fa7fb126a979b74c7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a53f9b69755737a6e964');
